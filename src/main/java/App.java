285ece67e6fa30e74ad899c8533669cacdd64908
Thomas Maurer <0876337@hr.nl>
2015-06-18 16:20:16 +0200

    Scenario 3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,24 +17,37 @@ public class App {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
 
-        scenarioAddUserAndAdvert(session);
+        scenarioAddUserAndAdvertWithLinkedCategories(session);
 
         session.getTransaction().commit();
         HibernateUtil.getSessionFactory().close();
 
     }
 
-    private static void scenarioAddUserAndAdvert(Session session){
+    private static void scenarioAddUserAndAdvertWithLinkedCategories(Session session){
         Date date = new Date();
         List<Category> categories = new ArrayList<Category>();
-        Gebruiker gebruiker = new Gebruiker("Thomas","Maurer","ckx@cks.com","root", null, null,null);
-        Advertentie advertentie = new Advertentie("PC te koop","2 jaar oud",300,true,date,null,gebruiker,categories,null);
+        List<Category> subcategories = new ArrayList<Category>();
+        Gebruiker verkoper = new Gebruiker("Thomas","Maurer","ckx@cks.com","root", null, null,null);
+        Advertentie advertentie = new Advertentie("PC te koop","2 jaar oud",300,true,date,null,verkoper,categories,null);
 
-        Category category = new Category("Computers",null,null);
-        categories.add(category);
+        Category parent1 = new Category("Computers",null,null);
+        Category parent = new Category("Beeld",subcategories,null);
+        Category subcategory = new Category("Televisie",null,null);
+        Category parent2 = new Category("Telefoon", null,null);
 
-        session.save(category);
-        session.save(gebruiker);
+        categories.add(parent1);
+        categories.add(parent2);
+        categories.add(parent);
+        subcategories.add(subcategory);
+
+        session.save(parent1);
+        session.save(parent);
+        session.save(parent2);
+        session.save(subcategory);
+        session.save(verkoper);
         session.save(advertentie);
     }
+
+
 }
